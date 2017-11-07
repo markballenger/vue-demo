38eef0b22151801adcae4eb6391ae6b2aedb5956
mark ballenger <mark.ballenger@e-hps.com>
2017-11-07 00:49:31 +0000

    step3 add search route

---
+++ src/components/Search.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar color="cyan" dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>Inbox</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>search</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-list two-line>
+          <template v-for="item in items">
+            <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+            <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+            <v-list-tile avatar v-else v-bind:key="item.title" @click="">
+              <v-list-tile-avatar>
+                <img v-bind:src="item.avatar">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.name"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+            { name: 'Jimi Hendrix' },
+            { name: 'PinkFloyd' }
+        ]
+      }
+    }
+  }
+</script>
