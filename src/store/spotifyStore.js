eb9595d94e1ee0d1aa387b6756673e09ee87ae39
mark ballenger <mark.ballenger@e-hps.com>
2017-11-07 01:32:42 +0000

    step6 call spotify for data

--- src/store/spotifyStore.js
+++ src/store/spotifyStore.js
@@ -1,22 +1,47 @@
-// import _ from 'lodash'
+import _ from 'lodash'
+import authHttp from '@/authHttp'
 
 /**
  * State
  */
 const state = {
   searchResults: [],
-  searchText: ''
+  searchText: '',
+  loading: false
 }
 
 /**
  * Actions
  */
 const actions = {
-  getSearchResults ({commit, state, rootState}) {
-    let data = [{ name: 'Jimi Hendrix' },
-            { name: 'Pink Floyd' }]
-    commit('get_search_results', data)
-  }
+  getSearchResults: _.debounce(function ({commit, state, rootState}) {
+    // if not logged in, set list empty
+    if (!rootState.spotifyAuth.accessToken) {
+      window.location = 'http://localhost:8080/login'
+      commit('get_search_results', [])
+    }
+
+    // update the spinny
+    this.dispatch('updateLoading', true)
+
+    // call the api
+    authHttp
+            .get(`search?type=artist,album&limit=50&q=${state.searchText || 'a'}`, rootState.spotifyAuth.accessToken)
+            .then(response => {
+              // flatten all the lists to one
+              let searchResults = _.flatten([
+                response.data.artists.items,
+                response.data.albums.items])
+              // sort it
+              searchResults = _.sortBy(searchResults, 'name')
+              searchResults = _.each(searchResults, function (x) { x.active = false })
+              commit('get_search_results', searchResults)
+              this.dispatch('updateLoading', false)
+            })
+  }, 300),
+  updateLoading: _.debounce(function ({commit}, loading) {
+    commit('update_loading', loading)
+  }, 200)
 }
 
 /**
@@ -25,6 +50,9 @@ const actions = {
 const mutations = {
   get_search_results (state, data) {
     state.searchResults = data
+  },
+  update_loading (state, loading) {
+    state.loading = loading
   }
 }
 
@@ -32,7 +60,8 @@ const mutations = {
  * Getters
  */
 const getters = {
-  searchResults: x => x.searchResults
+  searchResults: x => x.searchResults,
+  loading: x => x.loading
 }
 
 export default {
